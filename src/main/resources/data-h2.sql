10727c696bc57475ce2b6e80156adc448838e8c2
Rahul V <rahulv@live.ca>
2019-04-01 15:30:46 -0400

    Load seed data for dev profile.

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO BOOKMARK VALUES (1, 'www.google.com', 'google')
+INSERT INTO BOOKMARK VALUES (2, 'www.apple.com', 'apple')
+INSERT INTO BOOKMARK VALUES (3, 'www.facebook.com', 'facebook')
+INSERT INTO BOOKMARK VALUES (4, 'www.reddit.com', 'reddit')
